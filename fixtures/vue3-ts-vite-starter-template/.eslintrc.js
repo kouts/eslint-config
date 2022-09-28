52d9f63e18706d6550337f4ddf65bb4d0c12061c
kouts <koutsaftakis@gmail.com>
2022-09-28 09:40:16 +0300

    fix: default test configs to local location

--- fixtures/vue3-ts-vite-starter-template/.eslintrc.js
+++ fixtures/vue3-ts-vite-starter-template/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ['@kouts/eslint-config/vue3-typescript'],
+  // extends: ['@kouts/eslint-config/vue3-typescript'],
+  extends: ['../../vue3-typescript.js'],
   overrides: [
     {
       // Disable multi-word-component-names for pages and layouts
--- fixtures/vue3-ts-vite-starter-template/prettier.config.js
+++ fixtures/vue3-ts-vite-starter-template/prettier.config.js
@@ -1,1 +1,2 @@
-module.exports = require('@kouts/eslint-config/prettier.config.js')
+// module.exports = require('@kouts/eslint-config/prettier.config.js')
+module.exports = require('../../prettier.config.js')
