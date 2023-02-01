e333d29677c0de37b46992877a9be6410d8329e2
kouts <koutsaftakis@gmail.com>
2023-02-01 18:52:07 +0200

    test: updated fixtures npm packages, fixed jest 29 compatibility

--- fixtures/vue3-ts-vite-starter-template/jest.config.js
+++ fixtures/vue3-ts-vite-starter-template/jest.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   preset: 'ts-jest',
-  // Workaround for https://github.com/kulshekhar/ts-jest/issues/1648
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json'
-    }
-  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\js$': 'babel-jest',
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json'
+      }
+    ]
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   moduleFileExtensions: ['vue', 'js', 'ts'],
   moduleNameMapper: {
-    // Fix in order for vue-test-utils to work with Jest 28
-    // https://github.com/vuejs/vue-test-utils/issues/1975
-    '^@vue/test-utils$': require.resolve('@vue/test-utils'),
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-  coverageReporters: ['text', 'json-summary']
+  coverageReporters: ['text', 'json-summary'],
+  // Fix in order for vue-test-utils to work with Jest 29
+  // https://test-utils.vuejs.org/migration/#test-runners-upgrade-notes
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  }
 }
