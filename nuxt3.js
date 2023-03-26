c24dd0c81b56df66e9403b4d78426fe5fcc3223d
kouts <koutsaftakis@gmail.com>
2023-03-26 21:59:04 +0300

    fix: fixed imports sorting

--- nuxt3.js
+++ nuxt3.js
@@ -3,6 +3,9 @@ const base = require.resolve('./base.js')
 module.exports = {
   extends: [base, '@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   rules: {
+    // Conflicts with sort-imports-es6-autofix
+    'import/order': 'off',
+
     /* Typescript */
 
     // Prefer T[] instead of Array<T>
