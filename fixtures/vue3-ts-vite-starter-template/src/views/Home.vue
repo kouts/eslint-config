4321d456ac8792d5492ab7ea95bec6054d8faf70
kouts <koutsaftakis@gmail.com>
2024-07-21 22:34:33 +0300

    test: fixed fixture

--- fixtures/vue3-ts-vite-starter-template/src/views/Home.vue
+++ fixtures/vue3-ts-vite-starter-template/src/views/Home.vue
@@ -3,18 +3,19 @@
     <div class="text-center">
       <img alt="Vue logo" src="@/assets/logo.png" class="text-center" />
     </div>
-    <hello-world msg="Welcome to Vue.js 3 Vite starter template" />
+    <hello-world :msg="msg" />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home'
-}
-</script>
-
-<script setup>
-// https://github.com/vuejs/eslint-plugin-vue/issues/1577
-// eslint-disable-next-line import/first
+<script setup lang="ts">
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+
+defineOptions({
+  name: 'Home'
+})
+
+const msg = ref<string>('Welcome to Your Vue.js + Vite App')
+
+msg.value = 4
 </script>
